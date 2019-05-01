7e9f3ea9d3a4b27c43895d06d8673ac14fe3db7b
Logon PF Fiap <logonpflocal@fiap.com.br>
2019-04-30 22:26:36 -0300

    JSF navegação e template

--- 17-JSF-JPA/src/br/com/fiap/bean/FilmeBean.java
+++ 17-JSF-JPA/src/br/com/fiap/bean/FilmeBean.java
@@ -7,7 +7,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import br.com.fiap.dao.FilmeDAO;
@@ -17,7 +17,7 @@ import br.com.fiap.exception.CommitException;
 import br.com.fiap.singleton.EntityManagerFactorySingleton;
 
 @ManagedBean
-@ViewScoped
+@RequestScoped 
 public class FilmeBean implements Serializable {
 
 	private Filme filme;
@@ -31,24 +31,49 @@ public class FilmeBean implements Serializable {
 		dao = new FilmeDAOImpl(EntityManagerFactorySingleton.getInstance().createEntityManager());
 	}
 	
+	public String remover(int codigo) {
+		try {
+			dao.remover(codigo);
+			dao.commit();
+			adicionarMensagem("Removido!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			adicionarMensagem("Erro..");
+		}
+		return "lista-filme?faces-redirect=true";
+	}
+	
 	public List<Filme> listar(){
 		return dao.listar();
 	}
 
-	public void salvar() {
+	public String salvar() {
 		try {
-			dao.cadastrar(filme);
-			dao.commit();
-			adicionarMensagem("Cadastrado!");
+			if (filme.getCodigo() == 0) {
+				dao.cadastrar(filme);
+				dao.commit();
+				adicionarMensagem("Cadastrado!");
+			}else {
+				dao.atualizar(filme);
+				dao.commit();
+				adicionarMensagem("Atualizado!");
+			}
+			return "lista-filme?faces-redirect=true"; //navega para a página lista-filme.xhtml
 		} catch (CommitException e) {
 			e.printStackTrace();
 			adicionarMensagem("Erro..");
+			return "cadastro-filme";
 		}
 	}
 	
 	private void adicionarMensagem(String mensagem) {
 		FacesMessage msg = new FacesMessage(mensagem);
 		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+		//Mantém as mensagens após um redirect
+		FacesContext.getCurrentInstance().getExternalContext()
+			.getFlash().setKeepMessages(true);
+		
 	}
 	
 	public Filme getFilme() {
